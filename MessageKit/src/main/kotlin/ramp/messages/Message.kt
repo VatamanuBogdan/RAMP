ace1b66b75d928282d2a8f5ee47db3ac6da1289a
Vatamanu Bogdan <vatamanu.bogdan41@gmail.com>
2022-05-08 17:40:59 +0300

    Message Kit: Added minimal message structure and serialization

--- MessageKit/src/main/kotlin/ramp/messages/Message.kt
+++ MessageKit/src/main/kotlin/ramp/messages/Message.kt
@@ -1,4 +1,23 @@
 package ramp.messages
+import kotlinx.serialization.*
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
 
-class Message {
+@Serializable
+data class MessageInfo(val sender: String, val receiver: String, val broadcast: Boolean = false)
+
+@Serializable
+sealed class Message {
+    abstract val info: MessageInfo
+}
+
+@Serializable
+data class WorkPublishMessage(override val info: MessageInfo, val work: String): Message()
+
+object MessageSerializer : JsonContentPolymorphicSerializer<Message>(Message::class) {
+    override fun selectDeserializer(element: JsonElement) = when {
+        "work" in element.jsonObject -> WorkPublishMessage.serializer()
+        else -> Message.serializer()
+    }
 }
