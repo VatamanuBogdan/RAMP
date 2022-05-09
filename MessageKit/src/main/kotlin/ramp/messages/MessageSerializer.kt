75f59c5795199b8c4dd4495be8bcab5971da8794
Vatamanu Bogdan <vatamanu.bogdan41@gmail.com>
2022-05-09 17:32:17 +0300

    MessageKit: Added load message

--- MessageKit/src/main/kotlin/ramp/messages/MessageSerializer.kt
+++ MessageKit/src/main/kotlin/ramp/messages/MessageSerializer.kt
@@ -7,6 +7,7 @@ import kotlinx.serialization.json.jsonObject
 object MessageSerializer : JsonContentPolymorphicSerializer<Message>(Message::class) {
     override fun selectDeserializer(element: JsonElement) = when {
         "work" in element.jsonObject -> WorkPublishMessage.serializer()
+        "loadValue" in element.jsonObject -> LoadingMessage.serializer()
         "robotId" in element.jsonObject -> LoginMessage.serializer()
         else -> Message.serializer()
     }
--- MessageKit/src/main/kotlin/ramp/messages/Messages.kt
+++ MessageKit/src/main/kotlin/ramp/messages/Messages.kt
@@ -6,4 +6,7 @@ import kotlinx.serialization.Serializable
 data class LoginMessage(override val transport: MessageTransport, val robotId: String): Message()
 
 @Serializable
+data class LoadingMessage(override val transport: MessageTransport, val loadValue: Long): Message()
+
+@Serializable
 data class WorkPublishMessage(override val transport: MessageTransport, val work: String): Message()
